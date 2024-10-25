cf1d8ed244be229fd438ed5e850e15c56631f75d
Danijela Oljaca <danijela.oljaca@nfinnova.com>
2024-10-25 17:40:35 +0200

    Domaci Vezba 19

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String [] Objekti = { "Bicycle","Car","Mobile phone"};
+        String [] Model = {"BMX","Renault 4","iPhone"};
+        double [] Cena = {150.55,100.0,500.0};
+
+        System.out.println(Model[0]+" is type "+Objekti[0]+" and it costs "+ Cena[0]);
+        System.out.println(Model[1]+" is type "+Objekti[1]+" and it costs "+ Cena[1]);
+        System.out.println(Model[2]+" is type "+Objekti[2]+" and it costs "+ Cena[2]);
+
+    }
+}
